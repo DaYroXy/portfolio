a867909a3133e8ca1b0a357b0207a0ea74bfaebf
DaYroXy <tmureckless@gmail.com>
2023-12-08 02:19:16 +0200

    seo reoptimization

---
+++ app/sitemap.ts
@@ -0,0 +1,13 @@
+
+const sitemap = () => {
+    const baseUrl = "https://dayroxy.online"
+
+    return [
+        {
+            url: baseUrl,
+            lastModified: new Date(),
+        }
+    ]
+}
+
+export default sitemap
